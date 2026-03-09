d27193059a47b50306574012afd0e93443e55b22
UD-raitaro <yudaisato82@gmail.com>
2026-03-10 00:45:39 +0900

    Add files via upload

---
+++ yu-yudaiweddingphoto/content/seating-data.js
@@ -0,0 +1,15 @@
+window.seatingConfig = {
+  heading: "席表",
+  intro: "各卓名を押すと、対応する席表画像が表示されます。",
+  tables: [
+    { name: "1卓", image: "assets/images/seating/table-01.svg", alt: "1卓の席表" },
+    { name: "2卓", image: "assets/images/seating/table-02.svg", alt: "2卓の席表" },
+    { name: "3卓", image: "assets/images/seating/table-03.svg", alt: "3卓の席表" },
+    { name: "4卓", image: "assets/images/seating/table-04.svg", alt: "4卓の席表" },
+    { name: "5卓", image: "assets/images/seating/table-05.svg", alt: "5卓の席表" },
+    { name: "6卓", image: "assets/images/seating/table-06.svg", alt: "6卓の席表" },
+    { name: "7卓", image: "assets/images/seating/table-07.svg", alt: "7卓の席表" },
+    { name: "8卓", image: "assets/images/seating/table-08.svg", alt: "8卓の席表" },
+    { name: "9卓", image: "assets/images/seating/table-09.svg", alt: "9卓の席表" }
+  ]
+};
