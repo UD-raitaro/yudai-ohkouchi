5a0475658ca5e719bdcd5a91c42b1c33097bd2b1
UD-raitaro <yudaisato82@gmail.com>
2026-03-10 01:13:01 +0900

    Update seating-data.js

--- yu-yudaiweddingphoto/content/seating-data.js
+++ yu-yudaiweddingphoto/content/seating-data.js
@@ -2,14 +2,15 @@ window.seatingConfig = {
   heading: "席表",
   intro: "各卓名を押すと、対応する席表画像が表示されます。",
   tables: [
-    { name: "1卓", image: "assets/images/seating/table-01.svg", alt: "1卓の席表" },
-    { name: "2卓", image: "assets/images/seating/table-02.svg", alt: "2卓の席表" },
-    { name: "3卓", image: "assets/images/seating/table-03.svg", alt: "3卓の席表" },
-    { name: "4卓", image: "assets/images/seating/table-04.svg", alt: "4卓の席表" },
-    { name: "5卓", image: "assets/images/seating/table-05.svg", alt: "5卓の席表" },
-    { name: "6卓", image: "assets/images/seating/table-06.svg", alt: "6卓の席表" },
-    { name: "7卓", image: "assets/images/seating/table-07.svg", alt: "7卓の席表" },
-    { name: "8卓", image: "assets/images/seating/table-08.svg", alt: "8卓の席表" },
-    { name: "9卓", image: "assets/images/seating/table-09.svg", alt: "9卓の席表" }
+    { name: "Mt'HINATA", image: "assets/images/seating/table-01.svg", alt: "1卓の席表" },
+    { name: "Mt'Aka", image: "assets/images/seating/table-02.svg", alt: "2卓の席表" },
+    { name: "Mt'Nishiho", image: "assets/images/seating/table-03.svg", alt: "3卓の席表" },
+    { name: "Mt'HO-KEN", image: "assets/images/seating/table-04.svg", alt: "4卓の席表" },
+    { name: "NISHITANZAWA", image: "assets/images/seating/table-05.svg", alt: "5卓の席表" },
+    { name: "HAYACHINE", image: "assets/images/seating/table-06.svg", alt: "6卓の席表" },
+    { name: "Mt'HAKU", image: "assets/images/seating/table-07.svg", alt: "7卓の席表" },
+    { name: "BO-NOMINE", image: "assets/images/seating/table-08.svg", alt: "8卓の席表" },
+    { name: "GOZAISHO", image: "assets/images/seating/table-09.svg", alt: "9卓の席表" }
+    { name: "Hachimantai", image: "assets/images/seating/table-09.svg", alt: "10卓の席表" }
   ]
 };
